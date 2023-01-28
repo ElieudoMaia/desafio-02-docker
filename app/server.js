cf8f3e6adc7de27c6240cdff3d40030ecce33707
Elieudo Maia <elieudo.maia@g4flex.com.br>
2023-01-28 11:32:23 -0300

    feat: handle correctly  app initialization and db queries

--- app/server.js
+++ app/server.js
@@ -1,30 +1,70 @@
-import http from 'node:http'
-import mysql from 'mysql'
+import http from 'node:http';
+import mysql from 'mysql';
 
 const dbConnection = mysql.createConnection({
-  host: 'localhost',
+  host: 'mysqldb',
   user: 'root',
   password: 'root',
-  database: 'nodedb'
-})
+  database: 'nodedb',
+  port: 3306,
+});
 
 const server = http.createServer(async (_req, res) => {
-  dbConnection.query('INSERT INTO users (name) VALUES (\'John\')')
+  const getPeople = () => {
+    return new Promise((resolve, reject) => {
+      dbConnection.query('SELECT * FROM people', (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World from server.js!')
-})
+  try {
+    const people = await getPeople();
 
-dbConnection.connect((err) => {
+    const peopleNamesAsString = people.map((person) => `- ${person.name}`).join('<br />');
+
+    const response = `<h1>Full Cycle Rocks!</h1><br />${peopleNamesAsString}`
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(response);
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Error: ' + error);
+  }
+});
+
+dbConnection.connect(async (err) => {
   if (err) {
-    console.error('Error connecting MySql: ' + err.stack)
-    return
+    console.error('Error connecting MySql: ' + err.stack);
+    process.exit(1);
   }
 
+  dbConnection.query(
+    'CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))',
+    (err) => {
+      if (err) {
+        console.error('Error creating table: ' + err.stack);
+        process.exit(1);
+      }
+
+      dbConnection.query('INSERT INTO people (name) VALUES ("John"), ("Mary"), ("Antony")', (err) => {
+        if (err) {
+          console.error('Error inserting data: ' + err.stack);
+          process.exit(1);
+        }
+      });
+    }
+  );
+
   server.listen(3333, () => {
-    console.log('Server running at http://localhost:3333/')
-  })
+    console.log('Server running at http://localhost:3333/');
+  });
   server.on('close', () => {
-    dbConnection.end()
-  })
-})
+    dbConnection.end();
+    process.exit(0);
+  });
+});
